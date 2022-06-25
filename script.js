cdf06d1edcc5c75b1a0bba725a9ca74b753db110
dtvaltvadze <dtvaltvadze@gmail.com>
2022-06-25 16:49:49 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//lesson 3 - ex.1
+
+let array = [5, 25, 89, 120, 36];
+// array.push('javascript', 'python');
+// array.unshift('html', 'css');
+// console.log(array);
+
+// let result = array.length;
+// console.log(array.length);
+
+// array.shift();
+// console.log(array);
+
+array.pop();
+console.log(array);
+
+
+
